2e530354881166cedfa70877cce2b45cb3d21ca4
nguyennghia0479 <nguyennghhia0479@gmail.com>
2024-07-22 18:32:30 +0700

    Add clone skill

--- Assets/Scripts/Managers/SkillManager.cs
+++ Assets/Scripts/Managers/SkillManager.cs
@@ -6,13 +6,16 @@ public class SkillManager : Singleton<SkillManager>
 {
     private DashSkill dashSkill;
     private SwordSkill swordSkill;
+    private CloneSkill cloneSkill;
 
     private void Start()
     {
         dashSkill = GetComponent<DashSkill>();
         swordSkill = GetComponent<SwordSkill>();
+        cloneSkill = GetComponent<CloneSkill>();
     }
 
     public DashSkill DashSkill { get { return dashSkill; } }
     public SwordSkill SwordSkill { get { return swordSkill; } }
+    public CloneSkill CloneSkill { get { return cloneSkill; } }
 }
